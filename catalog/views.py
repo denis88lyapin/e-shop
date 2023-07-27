def1ac071d9a642013564bb554e428862ca306d0
Denis_L <denis88lyapin@gmail.com>
2023-07-27 19:48:01 +0300

    added fixture to views.py

--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 
 def home(request):
-   return render(request, 'catalog/home.html')
+    return render(request, 'catalog/home.html')
+
 
 def contacts(request):
-   return render(request, 'catalog/contacts.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name} ({email}): {message}')
+    return render(request, 'catalog/contacts.html')
