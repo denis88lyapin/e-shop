4bcbf46a13752b46515d10a5c3de83640cfe4b70
Denis_L <denis88lyapin@gmail.com>
2023-08-09 20:12:16 +0300

    created model blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {
+    'null': True,
+    'blank': True
+}
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=150, verbose_name='заголовок')
+    slug = models.CharField(max_length=150, verbose_name='slug')
+    body = models.TextField(verbose_name='содержимое')
+    image = models.ImageField(upload_to='blog/', **NULLABLE, verbose_name='превью')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    is_published = models.BooleanField(default=True, verbose_name='признак публикации')
+    views_count = models.IntegerField(default=0, verbose_name='количество просмотров')
+
+    def __str__(self):
+        return f'{self.pk} ({self.title})'
