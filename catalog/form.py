7a6f4251a56c0dd4f958fd8aa433697b0e7483c6
Denis_L <denis88lyapin@gmail.com>
2023-08-25 12:48:23 +0300

    added VersionForm

--- catalog/form.py
+++ catalog/form.py
@@ -1,19 +1,29 @@
 from django import forms
-from catalog.models import Product
+from catalog.models import Product, Version
 
 
-class ProductForm(forms.ModelForm):
+class VisualMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
+class ProductForm(VisualMixin, forms.ModelForm):
     class Meta:
         model = Product
         fields = ('name', 'description', 'image', 'category', 'price')
 
-
     def clean_name(self):
-        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                           'радар']
         cleaned_data = self.cleaned_data.get('name')
         for word in forbidden_words:
             if word in cleaned_data.lower():
                 raise forms.ValidationError('Запрещенное название')
         return cleaned_data
 
+
+class VersionForm(VisualMixin, forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = ('version_num', 'version_name', 'version_activ')
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
 
-from catalog.form import ProductForm
+from catalog.form import ProductForm, VersionForm
 from catalog.models import Product, Contacts, Version
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 
@@ -47,7 +47,7 @@ class ProductsListView(ListView):
         context = super().get_context_data(**kwargs)
 
         for product in context['object_list']:
-            active_version = Version.objects.filter(product=product, version_activ=True).first()
+            active_version = Version.objects.filter(product=product, version_activ=True).last()
             if active_version:
                 product.active_version_number = active_version.version_num
                 product.active_version_name = active_version.version_name
@@ -68,7 +68,7 @@ class ProductDetailViev(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        active_version = Version.objects.filter(product=self.object, version_activ=True).first()
+        active_version = Version.objects.filter(product=self.object, version_activ=True).last()
         if active_version:
             context['active_version_number'] = active_version.version_num
             context['active_version_name'] = active_version.version_name
@@ -91,6 +91,7 @@ class CreateProductView(CreateView):
 class UpdateProductView(UpdateView):
     model = Product
     form_class = ProductForm
+    template_name = 'catalog/update_product.html'  # Создайте шаблон update_product.html
     success_url = reverse_lazy('catalog:home')
     extra_context = {
         'title': 'Изменить товар',
@@ -98,6 +99,31 @@ class UpdateProductView(UpdateView):
 
     def get_success_url(self):
         return reverse('catalog:detail_product', args=[self.object.pk])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['version_form'] = VersionForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        version_form = VersionForm(request.POST)
+
+        if form.is_valid() and version_form.is_valid():
+            return self.form_valid(form, version_form)
+        else:
+            return self.form_invalid(form, version_form)
+
+    def form_valid(self, form, version_form):
+        self.object = form.save()
+        version = version_form.save(commit=False)
+        version.product = self.object
+        version.save()
+        return redirect(self.get_success_url())
+
+    def form_invalid(self, form, version_form):
+        return self.render_to_response(self.get_context_data(form=form, version_form=version_form))
 
 
 class DeleteProductView(DeleteView):
